aff5d40b8f20ebdf34fb4048e33a647d1986ba22
CaneRig <cane@test.mail>
2024-12-23 23:16:58 +0300

    Fixed pipeline

--- effects_pipeline.sv
+++ effects_pipeline.sv
@@ -1,44 +1,53 @@
 module effects_pipline #(
      parameter bits_per_level = 12
 ) (
-     input clk, 
-	  input  [10: 0] gain_value,
-     input  [15: 0] sample, // to 12 bits
-     output [15: 0] out_sample
+     input clk,
+     input rst,
+
+     // effects parameters 
+	input  [10: 0] gain_value,
+
+
+     input          valid,
+     input  [12: 0] sample_in,
+     output [15: 0] sample_out
 );
 
-/// GATE 1
-     wire [15: 0] gate1_value;
+// Overdrive
+     logic [15  :0   ]   overdrive_in;
+
      flipflop#(
-     .size = 16
-) i_ff_in(
+          .size = 16
+     ) i_overdrive_ff (
           .clk(clk),
-          .data(sample),
-          .out(gate1_value)
+          .rst(rst),
+          .valid(valid),
+          .data(sample_in),
+          .out(overdrive_in)
      );
-     shortint signed distortion_in, gain, out;
 
-     assign distortion_in = shortint'(gate1_value);
-     assign gain = shortint'(gain_value);
+     shortint signed ss_ovrd_in, ss_gain;
 
-     wire [15: 0] gate2_in;
+     assign ss_ovrd_in   = shortint'(overdrive_in);
+     assign ss_gain      = shortint'(gain_value);
 
-     distortion #(bits_per_level)
-	     m_distr(distortion_in, gain, gate2_in);
+     overdrive #(
+          .bits_per_level = bits_per_level
+     ) i_ovrd (
+          .signal_in(ss_ovrd_in),
+          .gain(ss_gain),
+          .signal_out(overdrive_out)
+     );
 
-
-
-/// GATE 2
-     wire [15: 0] gate2_value;
+// Output
      flipflop#(
-     .size = 16
-) gate2(
+          .size = 16
+     ) i_output_ff (
           .clk(clk),
-          .data(gate2_in),
-          .out(gate2_value)
+          .rst(rst),
+          .valid(valid),
+          .data(overdrive_out),
+          .out(sample_out)
      );
-	  
-
-     assign out_sample = gate2_value;
      
 endmodule
