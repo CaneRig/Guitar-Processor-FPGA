a1d57d9272d460d8fc9101252adf3998bfa2ca81
CaneRig <originalomail@gmail.com>
2025-02-01 14:19:00 +0300

    Math multiplicaiton fix

--- util_modules/fixed_math.sv
+++ util_modules/fixed_math.sv
@@ -9,11 +9,8 @@ module fixed_multiply#(
     output logic signed[operand_size*2-1: 0] c
 );
 
-     logic [(operand_size)*2 - 1: 0] temp_result; // Temporary result to handle overflow
-     logic [(operand_size)*2 - 1: 0] partial_products [(operand_size) * 2]; // Array to store partial products
-
-     logic [(operand_size)*2 - 1: 0] a_extended;
-     logic [(operand_size)*2 - 1: 0] b_extended;
+     logic [(operand_size)*2 - 1: 0] signed a_extended;
+     logic [(operand_size)*2 - 1: 0] signed b_extended;
 
      signed_expand #(
           .operand_size(operand_size),
@@ -30,24 +27,7 @@ module fixed_multiply#(
           .out(b_extended)
      );   
 
-     // Generate block to calculate partial products
-     genvar i;
-     generate
-          for (i = 0; i < (operand_size) * 2; i = i + 1) begin : mult_loop
-               assign partial_products[i] = b_extended[i] ? (a_extended << i) : 0; // Shift and conditionally select
-          end
-     endgenerate
-
-     //  sum all partial products
-     always_comb begin
-          temp_result = 0;  
-          for (int j = 0; j < (operand_size)*2; j++) begin
-               temp_result = temp_result + partial_products[j]; // Accumulate partial products
-          end
-     end
-
-     // Assign the lower bits to the output
-     assign c = $signed(temp_result) >>> fractional_size;
+     assign c = $signed(a_extended * b_extended) >>> fractional_size;
 
 endmodule
 
