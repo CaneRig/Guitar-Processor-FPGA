198b459438cad004a5de318e5c4d86d71dee2595
CaneRig <originalomail@gmail.com>
2025-02-05 15:31:14 +0300

    TB fix

--- verification/tb_gain.sv
+++ verification/tb_gain.sv
@@ -2,30 +2,35 @@
 
 // TEST gain
 module tb_gain();
-shortint signed gain_mulA, gain_mulB;
-int gain_mulOut;
+int signed gain_mulA, gain_mulB;
+int signed gain_mulOut;
 int signed gain_expected;
 
 gain m_gain_mul(gain_mulA, gain_mulB, gain_mulOut) ;
 
+function int abs(input int a);
+     if(a < 0)
+          return -a;
+     else 
+          return a;
+endfunction
 
 initial begin // gain
      $display("gain - test");
      $dumpfile("test-gain.vcd");
-     $dumpvars(0,gain_mulA);
-     $dumpvars(0,gain_mulB);
-     $dumpvars(0,gain_mulOut);
+     $dumpvars;
 
-     for (integer i=-(2**16+1); i<(2**14); i+=2**7) begin
-          for (integer j=-(2**16+2); j<(2**14); j+=2**7) begin
+     for (integer signed i=-(2**14+1); i<(2**14); i+=2**7) begin
+          for (integer j=0; j<2**14; j+=2**7) begin
                #1
-               gain_mulA = i;
-               gain_mulB = j;
+               gain_mulA = $signed(i);
+               gain_mulB = $signed(j);
                #1
-               gain_expected = (int'(i)*int'(j) / (2**12));
-               if(gain_mulOut !== gain_expected && gain_mulOut !== gain_expected - 1) begin
+               gain_expected = ($signed(i*j) / (2**12));
+               if(abs(gain_expected - gain_mulOut) > 2) begin
                     $error("The gain, probably, not working (warning, currently there is error +/- 1 in checking the correctiness)");
                     $display("\nA=%d, B=%d, Answer = %d\ni=%d, j=%d, gain_expected = %d", gain_mulA, gain_mulB, gain_mulOut, i, j, gain_expected);
+                    $finish;
                end
           end
      end
