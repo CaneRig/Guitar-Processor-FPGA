8d7edd4b9e5efeab8b31f8aac6900a6116a70568
CaneRig <originalomail@gmail.com>
2025-02-15 01:05:47 +0300

    fixed_multiply exntension fix

--- util_modules/fixed_multiply.sv
+++ util_modules/fixed_multiply.sv
@@ -10,19 +10,21 @@ module fixed_multiply#(
     output logic signed[operand_size+expansion_size-1: 0] c
 );
 
-     logic signed [(operand_size + expansion_size) - 1: 0] a_extended;
-     logic signed [(operand_size + expansion_size) - 1: 0] b_extended;
+     localparam extension = (expansion_size < fractional_size)? fractional_size : expansion_size;
+
+     logic signed [(operand_size + extension) - 1: 0] a_extended;
+     logic signed [(operand_size + extension) - 1: 0] b_extended;
 
      signed_expand #(
           .operand_size(operand_size),
-          .expansion_size(operand_size)
+          .expansion_size(extension)
      ) i_a_expand (
           .in(a),
           .out(a_extended)
      );
      signed_expand #(
           .operand_size(operand_size),
-          .expansion_size(operand_size)
+          .expansion_size(extension)
      ) i_b_expand (
           .in(b),
           .out(b_extended)
