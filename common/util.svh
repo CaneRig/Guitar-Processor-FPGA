2b38a4ecca2d96cf8f02df955ab679caa4821bc6
CaneRig <originalomail@gmail.com>
2025-03-24 14:31:45 +0300

    Added fixed-point convertion and abs funciton

--- common/util.svh
+++ common/util.svh
@@ -20,5 +20,12 @@ function int fp2fx(input real value, input int frac_bits);
 endfunction
 
 function real fx2fp(int value, int frac_bits);
-    return real'(value) / (1 << frac_bits);
+    return real'(value) / real'(1 << frac_bits);
+endfunction
+
+function real abs(real a);
+     if(a < 0)
+          return -a;
+     else 
+          return a;
 endfunction
