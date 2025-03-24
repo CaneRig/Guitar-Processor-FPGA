45b8d81bd1d10b50e86944852dc2393b46f023bb
CaneRig <originalomail@gmail.com>
2025-03-24 14:35:48 +0300

    Total fix of overdrive_clamp testbench

--- verification/tb_overdrive_clamp.sv
+++ verification/tb_overdrive_clamp.sv
@@ -1,45 +1,75 @@
-`timescale 10s/1s
+`timescale 1ns/1ps
+`include "util.svh"
 
-// TEST overdrive_clamp
-function int multiply(int a, int b);
-     multiply = a * b / (2**12);
-endfunction
-
-function int overdrive_clamp_test(int value);
-     localparam One = 2**12;
-
-     if (value <= - One)
-          overdrive_clamp_test = - One / 2;
-     else if (value >= One)
-          overdrive_clamp_test =   One / 2;
-     else
-          overdrive_clamp_test = (multiply(multiply(value, value), value) + value * 3) / 4;
-endfunction
-
-module tb_overdrive_clamp();
-
-int overdrive_clamp_input;
-int overdrive_clamp_output;
-int overdrive_clamp_expected;
-
-overdrive_clamp m_overdrive_clamp(.i_sample(overdrive_clamp_input), .o_sample(overdrive_clamp_output));
-
-initial begin
-     $display("overdrive_clamp - test");
-     $dumpfile("test-overdrive_clamp.vcd");
-     $dumpvars;
-
-     for (integer i = -(2**16); i < (2**16); i+=2) begin
-          overdrive_clamp_input = i;
-          overdrive_clamp_expected = overdrive_clamp_test(i);
-          #1;
-          if(overdrive_clamp_output != overdrive_clamp_expected) begin
-               // $error("Error: wrong overdrive clamp error at %d: \n Expected:\t%d\n Found:\t%d\n", i, overdrive_clamp_expected, overdrive_clamp_output);
-               // $finish();
-          end
-          #1;
+function real oclamp(real real_input);
+     if (real_input < -1.0) begin
+          return -0.5;
+     end else if (real_input > 1.0) begin
+          return 0.5;
+     end else begin
+          return (3 * real_input + real_input ** 3) / 4;
      end
-     $finish();
-end
+endfunction
+
+module overdrive_clamp_tb();
+
+     // Parameters
+     parameter bits_per_level = 12;
+     parameter fxp_size = 16;
+     
+     localparam error_threshold = 0.01;
+
+     // Signals
+     logic signed[fxp_size-1:0] i_sample;
+     logic signed[fxp_size-1:0] o_sample;
+
+     // Varriables
+     real real_input;
+     real real_output;
+     real expected_output;
+     int signed fixed_input;
+     int signed fixed_output;
+     logic fail;
+
+     overdrive_clamp #(
+          .bits_per_level(bits_per_level),
+          .fxp_size(fxp_size)
+     ) dut (
+          .i_sample(i_sample),
+          .o_sample(o_sample)
+     );
+
+     initial begin
+          $display("overdrive_clamp - test");
+          $dumpfile("overdrive-clamp.vcd");
+          $dumpvars;
+
+          fail = '0;
+
+          for (real_input = -2.0; real_input <= 2.0; real_input += 0.1) begin
+               fixed_input = fp2fx(real_input, bits_per_level);
+               i_sample = fixed_input;
+
+               #1;
+
+               fixed_output = o_sample;
+               real_output = fx2fp(fixed_output, bits_per_level);
+
+               expected_output = oclamp(real_input);
+
+               #1;
+
+               if (abs(real_output - expected_output) >= error_threshold) begin
+                    $display("Error: Input = %f | %d, Output = %f, Expected = %f", real_input, fixed_input, real_output, expected_output);
+                    fail = '1;
+               end
+          end
+
+          if(fail)
+               $display("[TEST FAILED]");
+          else
+               $display("[TEST PASSED]");
+          $finish;
+     end
 
 endmodule
